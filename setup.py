62f0b6df3d3004a0c593cb3bdf76ad18f47bcc62
Daniel Walker <danielpdwalker@gmail.com>
2021-04-19 13:54:32 +0100

    Updated setup.py to include extra information

--- setup.py
+++ setup.py
@@ -2,16 +2,19 @@ from setuptools import find_packages, setup
 
 setup(
     name='matatika-meltano-report-converter',
-    version='0.1.0',
+    version='1.0',
+    description='A meltano custom utility to conver meltano reports into matatika yaml datasets',
+    author='DanielPDWalker',
+    url='https://www.matatika.com/',
     py_modules=['matatika_meltano_report_converter'],
     entry_points='''
         [console_scripts]
         matatika-meltano-report-converter=matatika_meltano_report_converter:matatika_convert_reports
     ''',
-    packages=['matatika_meltano_report_converter'],
-    include_package_data=True,
     install_requires=[
           'pyyaml==5.4.1',
           'meltano'
     ],
+    packages=['matatika_meltano_report_converter'],
+    include_package_data=True,
 )
