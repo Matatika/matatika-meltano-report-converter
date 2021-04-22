21f5eb3806a47beb9c7e37651e8e42ebf6d3cf7b
Daniel Walker <danielpdwalker@gmail.com>
2021-04-22 13:39:50 +0100

    Added linter black

--- matatika_meltano_report_converter/__init__.py
+++ matatika_meltano_report_converter/__init__.py
@@ -3,35 +3,37 @@ import yaml
 import os
 
 from meltano.api.controllers.sql_helper import SqlHelper
-from meltano.core.m5o.m5o_collection_parser import M5oCollectionParser, M5oCollectionParserTypes
-from meltano.core.m5o.m5o_file_parser import MeltanoAnalysisFileParser
+
 
 def chartjs_chart_type(chartjs_type):
-        # Lowercase first character of chart type
-        chartjs_type = chartjs_type[0].lower() + chartjs_type[1:]
-        chartjs_type = chartjs_type.replace("Chart", "")
-        if chartjs_type == "bar":
-            chartjs_type = "horizontalBar"
-        if chartjs_type == "verticalBar":
-            chartjs_type = "bar"
-        return chartjs_type
+    # Lowercase first character of chart type
+    chartjs_type = chartjs_type[0].lower() + chartjs_type[1:]
+    chartjs_type = chartjs_type.replace("Chart", "")
+    if chartjs_type == "bar":
+        chartjs_type = "horizontalBar"
+    if chartjs_type == "verticalBar":
+        chartjs_type = "bar"
+    return chartjs_type
+
 
 def generate_query(design_helper, sql_helper, report):
-        query_payload = report["query_payload"]
-        sql_dict = sql_helper.get_sql(design_helper, query_payload)
-        outgoing_sql = sql_dict["sql"]
-        return outgoing_sql
+    query_payload = report["query_payload"]
+    sql_dict = sql_helper.get_sql(design_helper, query_payload)
+    outgoing_sql = sql_dict["sql"]
+    return outgoing_sql
 
 
 def matatika_convert_reports():
     report_list = []
     try:
-        report_list = [file for file in os.listdir('./analyze/reports') if file[-4:] == '.m5o']
+        report_list = [
+            file for file in os.listdir("./analyze/reports") if file[-4:] == ".m5o"
+        ]
     except FileNotFoundError:
         print("analyze/reports directory not found in this meltano project.")
-    
+
     for file in report_list:
-        with open('./analyze/reports/'+file, 'r') as open_file:
+        with open("./analyze/reports/" + file, "r") as open_file:
             data = json.load(open_file)
             sql_helper = SqlHelper()
             m5oc = sql_helper.get_m5oc_topic(data["namespace"], data["model"])
@@ -41,24 +43,18 @@ def matatika_convert_reports():
             title = full_design["label"]
             description = full_design["description"]
             visualisation = {
-                "chartjs-chart": {
-                    "chartType": chartjs_chart_type(data["chart_type"])
-                }
-                #'google-chart': {"chartType": self.google_chart_type(data["chart_type"]), "options": {"title": title}}
+                "chartjs-chart": {"chartType": chartjs_chart_type(data["chart_type"])}
             }
 
             sql_query = generate_query(design_helper, sql_helper, data)
 
-            # If you pass dicts straight to the yaml dump you get the yaml teired layout rather than a json object to pass. 
-            # This means that in the yaml the metadata and visulisation will have single quotes around them to avoid the above.
+            # If you pass dicts straight to the yaml dump you get the yaml teir-ed layout rather than a json object to pass.
+            # This means that in the yaml the metadata and visulisation will have single quotes around them to avoid the above tier-ing.
             visualisation_jsonstr = json.dumps(visualisation)
-
-            #full_design_jsonstr = json.dumps(full_design)
-
             matatika_metadata_str = matatika_metadata_builder(full_design, sql_query)
 
             dataset = {
-                "source": None, 
+                "source": None,
                 "title": title,
                 "questions": None,
                 "description": description,
@@ -66,19 +62,23 @@ def matatika_convert_reports():
                 "visualisation": visualisation_jsonstr,
                 "query": sql_query,
             }
-            
 
             datasets = {"datasets": {data["slug"]: dataset}}
 
-            if not os.path.exists('converted_meltano_reports/'):
-                os.mkdir('converted_meltano_reports/')
+            if not os.path.exists("converted_meltano_reports/"):
+                os.mkdir("converted_meltano_reports/")
 
-            with open(f'converted_meltano_reports/{data["slug"]}.yaml', 'w') as output:
-                yaml.dump(datasets, output, sort_keys=False, encoding='utf8')
+            with open(f'converted_meltano_reports/{data["slug"]}.yaml', "w") as output:
+                yaml.dump(datasets, output, sort_keys=False, encoding="utf8")
+
 
 def matatika_metadata_builder(full_design, sql_query):
-    
-    matatika_metadata = {"name": None, "label": None, "related_table": {"columns": {}, "aggregates": {}}}
+
+    matatika_metadata = {
+        "name": None,
+        "label": None,
+        "related_table": {"columns": {}, "aggregates": {}},
+    }
 
     try:
         matatika_metadata["name"] = full_design["name"]
@@ -89,22 +89,36 @@ def matatika_metadata_builder(full_design, sql_query):
         matatika_metadata["label"] = full_design["label"]
     except:
         matatika_metadata["label"] = None
-    
-    
+
     matatika_metadata_columns = {"columns": []}
     for column in full_design["related_table"]["columns"]:
         if column["name"] in sql_query:
-            matatika_metadata_columns["columns"].append({"name": column["name"], "description": column["description"], "label": column["label"]})
+            matatika_metadata_columns["columns"].append(
+                {
+                    "name": column["name"],
+                    "description": column["description"],
+                    "label": column["label"],
+                }
+            )
 
     matatika_metadata_aggregates = {"aggregates": []}
     for aggregate in full_design["related_table"]["aggregates"]:
         if aggregate["name"] in sql_query:
-            matatika_metadata_aggregates["aggregates"].append({"name": aggregate["name"], "description": aggregate["description"], "label": aggregate["label"]})
-
+            matatika_metadata_aggregates["aggregates"].append(
+                {
+                    "name": aggregate["name"],
+                    "description": aggregate["description"],
+                    "label": aggregate["label"],
+                }
+            )
 
     try:
-        matatika_metadata["related_table"]["columns"] = matatika_metadata_columns["columns"]
-        matatika_metadata["related_table"]["aggregates"] = matatika_metadata_aggregates["aggregates"]
+        matatika_metadata["related_table"]["columns"] = matatika_metadata_columns[
+            "columns"
+        ]
+        matatika_metadata["related_table"]["aggregates"] = matatika_metadata_aggregates[
+            "aggregates"
+        ]
     except:
         matatika_metadata["related_table"]["columns"] = None
         matatika_metadata["related_table"]["aggregates"] = None
@@ -113,5 +127,5 @@ def matatika_metadata_builder(full_design, sql_query):
     return matatika_metadata_str
 
 
-if __name__ == '__init__':
+if __name__ == "__init__":
     matatika_convert_reports()
